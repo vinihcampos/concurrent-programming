f307d566c29580dbeaffe25595cb3a5ff00f2101
Vinícius Campos Tinoco Ribeiro <viniciuscampos120@gmail.com>
2017-10-06 02:18:37 -0300

    Include all statistic data information

--- bin/run_concurrent.sh
+++ bin/run_concurrent.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
-#for i in 4 8 16 32 64 128 256 512 1024 2048
-#do
-#	for k in 5 10 20 30 40 50 60 70 80 90 100 150 200 250 300 350 400 450 500
-#	do	
-#		for j in {1..20}
-#		do
-#			./multimat_concorrente $i $k
-#		done
-#	done
-#done
-
-for i in 2048
+for i in 4 8 16 32 64 128 256 512 1024 2048
 do
-	for j in 20 30 40 50 60 70 80 90 100 150 200 250 300 350 400 450 500
-	do
-		for k in {1..20}
-       	do
-           ./multimat_concorrente $i $j
-       	done
+	for k in 5 10 20 30 40 50 60 70 80 90 100 150 200 250 300 350 400 450 500
+	do	
+		for j in {1..20}
+		do
+			./multimat_concorrente $i $k
+		done
 	done
 done
